9675faed94911f8cf779d6a3b1bd58aac38c2770
Suman Kumar <sumancsnit@gmail.com>
2020-03-21 14:39:28 +0530

    new changes

--- src/HomePage/Components/HomePage.js
+++ src/HomePage/Components/HomePage.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Container, Typography, Box, Grid } from '@material-ui/core';
+import styles from '../Styles/styles ';
 
-export default class HomePage extends Component {
+class HomePage extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <>
-        
+        <Container className={classes.mainComtainer} maxWidth='lg'>
+          {/* <Box className={classes.searchBarWrapper}></Box> */}
+          <Grid container justify='center' alignItems='center' spacing={3}>
+            <Grid item xs={12}>
+              suman
+            </Grid>
+          </Grid>
+        </Container>
       </>
-    )
+    );
   }
 }
+
+export default withStyles(styles)(HomePage);
---
+++ src/HomePage/index.js
@@ -0,0 +1,4 @@
+import HomePage  from './Components/HomePage';
+
+export default HomePage
+
